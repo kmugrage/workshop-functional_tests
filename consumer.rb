44c525a49197dfec8fa42fbfa03367d4065b2005
Workshop User <none@example.com>
2014-06-13 16:31:04 -0700

    updated to phantomjs

--- consumer.rb
+++ consumer.rb
@@ -4,10 +4,13 @@ require "watir"
 
 test_site = "http://qa-consumer.work.shop"
 
-browser = Watir::Browser.new
+browser = Watir::Browser.new :phantomjs
 
 puts "Starting test"
 
 browser.goto test_site
 
+browser.screenshot.save "consumer.png"
+
+puts "Ending test"
 browser.close
