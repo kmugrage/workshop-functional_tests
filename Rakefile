501c025221ba8d0118bcb836787ebb7d837b47ff
Workshop User <none@example.com>
2014-06-13 17:17:04 -0700

    added b2b test

--- Rakefile
+++ Rakefile
@@ -1,4 +1,11 @@
+task	:run_all => [:consumer_test, :b2b_test]
+
 task	:consumer_test do
   watir = "./consumer.rb"
+  sh "#{watir}"
+end
+
+task	:b2b_test do
+  watir = "./b2b.rb"
   sh "#{watir}"
 end
---
+++ b2b.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby
+
+require "watir"
+require "test/unit"
+
+class VerifyConsumer < Test::Unit::TestCase
+
+def setup
+ @browser ||= Watir::Browser.new :phantomjs
+end
+
+def teardown
+ @browser.close
+end
+
+def test_search
+ @browser.goto "http://qa-b2b.work.shop"
+ assert @browser.text.include? "B2B"
+end
+
+end
